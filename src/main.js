fa9cdd23602ff5da8c73cc1cdd95011ecbd9b8cc
sandoramix <sandoramixer61@gmail.com>
2022-04-28 23:53:07 +0200

    cleaned code

--- src/main.js
+++ src/main.js
@@ -2,41 +2,6 @@ import App from './App.svelte';
 import { writable } from "svelte/store";
 
 
-
-
-export var user = writable({
-    access_token: null,
-    id: null,
-    email: null
-});
-export const api = `http://localhost/api/`
-export const leaflet_maps = `${api}/maps/z_x_y.jpg`;
-
-
-export function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    let expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-
-export function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return null;
-}
-
 const app = new App({
     target: document.body,
 });
