6b968bb85fe1ba5497cddaf388271c8801fc3460
sandoramix <sandoramixer61@gmail.com>
2022-07-06 18:03:54 +0200

    fixed pannel's erros

--- src/components/IGFishes.svelte
+++ src/components/IGFishes.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { createEventDispatcher } from "svelte";
+	import { createEventDispatcher, onMount } from "svelte";
 
 	var event = createEventDispatcher();
 
@@ -8,16 +8,27 @@
 	export var mapFishes = [];
 	export var mapTrophies = [];
 
-	$: visible = currentlyShowingFishes.length !== 0;
+	var visible = false;
 	var input;
 
-	$: currentlyShowingFishes = mapTrophiesToggler ? mapTrophies : mapFishesToggler ? mapFishes : allFishes;
+	$: currentlyShowingFishes = mapT && mapTrophies.length !== 0 ? mapTrophies : mapF && mapFishes.length !== 0 ? mapFishes : allFishes;
+	$: currentlyFilteredShowingFishes = currentlyShowingFishes.filter((fish) =>
+		currentLanguage.value === "ITA"
+			? fish.name_it.toLowerCase().includes(input ? input.toLowerCase() : ``)
+			: fish.name.toLowerCase().includes(input ? input.toLowerCase() : ``),
+	);
+	var mapT = false;
+	$: mapTAvaiable = mapTrophies.length !== 0;
 
-	$: currentlyFilteredShowingFishes = currentlyShowingFishes.filter((fish) => fish.name.toLowerCase().includes(input ? input.toLowerCase() : ``));
-
-	var mapTrophiesToggler = false;
-	var mapFishesToggler = true;
-
+	var mapF = true;
+	$: mapFAviable = mapFishes.length !== 0 && !mapT;
+	export const clearChecks = () => {
+		mapT = false;
+		mapF = false;
+	};
+	export const toggle = () => {
+		visible = !visible;
+	};
 	// //EVENTS
 
 	function onFocusToggle(flag) {
@@ -25,77 +36,120 @@
 	}
 </script>
 
-{#if visible}
-	<div class="container">
-		<div class="header">
-			<h3>{currentLanguage.fish}</h3>
-			<input
-				on:focus={() => onFocusToggle(true)}
-				on:blur={() => onFocusToggle(false)}
-				type="text"
-				class="search_fish"
-				bind:value={input}
-				placeholder={currentLanguage.search_fish}
-			/>
+<div class={`container ${visible ? `` : `hidden`}`}>
+	<div class="header">
+		<h3>{currentLanguage.fish}</h3>
+		<input
+			on:focus={() => onFocusToggle(true)}
+			on:blur={() => onFocusToggle(false)}
+			type="text"
+			class="search_fish"
+			bind:value={input}
+			placeholder={currentLanguage.search_fish}
+		/>
 
-			<div class="filters">
-				<label class="filter {mapTrophies.length == 0 ? `disabled` : ``}" value="trophy" for="trophy_toggler"
-					><hr class="line" />
-					<input
-						type="checkbox"
-						disabled={mapTrophies.length == 0}
-						bind:checked={mapTrophiesToggler}
-						id="trophy_toggler"
-						name="filter"
-					/>{currentLanguage.this_map_trophies}</label
-				>
+		<div class="filters">
+			<label class="filter {!mapTAvaiable ? `disabled` : ``}" value="trophy" for="trophy_toggler"
+				><hr class="line" />
+				<input
+					class={`${mapT}`}
+					type="checkbox"
+					disabled={!mapTAvaiable}
+					bind:checked={mapT}
+					id="trophy_toggler"
+					name="filter"
+				/>{currentLanguage.this_map_trophies}</label
+			>
 
-				<label class="filter {mapTrophiesToggler ? `disabled` : ``}" value="fish" for="fish_toggler"
-					><hr class="line" />
-					<input
-						type="checkbox"
-						disabled={mapTrophiesToggler || mapFishes.length == 0}
-						bind:checked={mapFishesToggler}
-						id="fish_toggler"
-						name="filter"
-					/>{currentLanguage.this_map_fish}</label
-				>
-			</div>
+			<label class="filter {(mapT && mapTrophies.length !== 0) || mapFishes.length === 0 ? `disabled` : ``}" value="fish" for="fish_toggler"
+				><hr class="line" />
+				<input
+					type="checkbox"
+					disabled={(mapT && mapTrophies.length !== 0) || mapFishes.length === 0}
+					bind:checked={mapF}
+					id="fish_toggler"
+					name="filter"
+				/>{currentLanguage.this_map_fish}</label
+			>
 		</div>
-		<ul id="fishes">
-			{#each currentlyFilteredShowingFishes as fish}
-				<li class="item">
-					<span class="fish">{currentLanguage.value == "ITA" ? fish.name_it : fish.name}</span>
-					<div class="fish-sub">
-						<div>
-							<img src="images/trophy.png" alt="Trophy" />
-							<span>{fish.trophy != null ? (fish.trophy < 1000 ? `${fish.trophy}gr` : `${fish.trophy / 1000}kg`) : `?`} </span>
-						</div>
-						<div>
-							<img src="images/super-trophy.png" alt="Trophy" />
-							<span>
-								{fish.super_trophy != null
-									? fish.super_trophy < 1000
-										? `${fish.super_trophy}gr`
-										: `${fish.super_trophy / 1000}kg`
-									: `?`}
-							</span>
-						</div>
-					</div>
-				</li>
-			{/each}
-		</ul>
 	</div>
-{/if}
+	<ul id="fishes">
+		{#each currentlyFilteredShowingFishes as fish}
+			<li class="item">
+				<span class="fish">{currentLanguage.value == "ITA" ? fish.name_it : fish.name}</span>
+				<div class="fish-sub">
+					<div>
+						<img src="images/trophy.png" alt="Trophy" />
+						<span>{fish.trophy != null ? (fish.trophy < 1000 ? `${fish.trophy}gr` : `${fish.trophy / 1000}kg`) : `?`} </span>
+					</div>
+					<div>
+						<img src="images/super-trophy.png" alt="Trophy" />
+						<span>
+							{fish.super_trophy != null
+								? fish.super_trophy < 1000
+									? `${fish.super_trophy}gr`
+									: `${fish.super_trophy / 1000}kg`
+								: `?`}
+						</span>
+					</div>
+				</div>
+			</li>
+		{/each}
+	</ul>
+	<div
+		class="toggler"
+		passive:true
+		on:click={() => {
+			visible = !visible;
+		}}
+	>
+		F<span style="color:whitesmoke;" />
+	</div>
+</div>
 
 <style>
+	.toggler {
+		position: absolute;
+		width: 3rem;
+		height: 7rem;
+		top: 0;
+		left: calc(var(--map-left-panel-width) - 1px);
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: var(--red-dark-color);
+
+		cursor: pointer;
+		padding: 0.5rem;
+		text-align: center;
+
+		border-top-right-radius: 5px;
+		border-bottom-right-radius: 5px;
+		user-select: none;
+		font-weight: bold;
+
+		border-top: 1px solid var(--outline-color);
+		border-bottom: 1px solid var(--outline-color);
+		border-right: 1px solid var(--outline-color);
+
+		transition: left 0.5s ease-in-out;
+		z-index: 65;
+	}
 	.header {
-		border-bottom: solid 1px var(--purple-outline-color);
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		gap: 0.25em;
+		height: 7rem;
+		border-bottom: solid 1px var(--orange);
 		padding: 0.25rem;
+		background-color: #07001b;
 	}
 	.filters {
 		display: flex;
-		background-color: #0e001d;
+		align-items: center;
 		justify-content: space-around;
 	}
 	.filter {
@@ -154,9 +208,19 @@
 		border-top: 1px solid var(--outline-color);
 		border-bottom: 1px solid var(--outline-color);
 		border-right: 1px solid var(--outline-color);
+		position: fixed;
+		top: 30%;
+		height: 51vh;
+		z-index: 60;
+		transition: transform 0.3s ease-in-out;
+	}
+	.container.hidden {
+		transform: translateX(-100%);
 	}
 
 	#fishes {
+		height: calc(50vh - 7rem);
+		max-height: calc(51vh - 7rem);
 		width: 100%;
 		user-select: none;
 		list-style: none;
@@ -167,19 +231,18 @@
 		justify-content: start;
 		font-size: 19px;
 		height: inherit;
-		max-height: 30vh;
 
 		overflow-y: auto;
 	}
 	.item {
 		width: inherit;
-		border-top: 1px solid var(--primary-color);
+		border-bottom: 1px solid var(--primary-color);
 		display: flex;
-
+		align-items: center;
 		justify-content: start;
 	}
 	.item:nth-child(odd) {
-		background-color: #000;
+		background-color: rgb(13, 0, 19);
 	}
 
 	.fish {
@@ -195,7 +258,7 @@
 		gap: 0.125rem;
 		justify-content: center;
 		font-size: 16px;
-		/* color: rgb(0, 159, 233); */
+		text-align: end;
 		color: var(--contrast-color);
 		width: 30%;
 
@@ -213,6 +276,8 @@
 	.fish-sub span {
 		width: 75%;
 		user-select: text;
+		margin-right: 10%;
+		font-weight: bold;
 	}
 	.fish-sub img {
 		width: 25%;
